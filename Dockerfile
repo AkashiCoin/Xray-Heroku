9a8a3bd7970e50550fab590732d7c9a170f45ac6
鸿煊 <643125846@qq.com>
2021-09-22 12:13:40 +0800

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,13 @@
-FROM alpine:latest
+FROM alpine:edge
 
-ADD entrypoint.sh /opt/entrypoint.sh
+RUN apk update && \
+    apk add --no-cache ca-certificates caddy tor wget && \
+    wget -O Xray-linux-64.zip  https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip  && \
+    unzip Xray-linux-64.zip && \
+    chmod +x /xray && \
+    rm -rf /var/cache/apk/*
 
-RUN apk add --no-cache --virtual .build-deps ca-certificates curl \
- && chmod +x /opt/entrypoint.sh
+ADD start.sh /start.sh
+RUN chmod +x /start.sh
 
-ENTRYPOINT ["sh", "-c", "/opt/entrypoint.sh"]
+CMD /start.sh
---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# configs
+mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
+wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
+wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
+wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/xray.json
+
+# storefiles
+mkdir -p /usr/share/caddy/$AUUID && wget -O /usr/share/caddy/$AUUID/StoreFiles $StoreFiles
+wget -P /usr/share/caddy/$AUUID -i /usr/share/caddy/$AUUID/StoreFiles
+
+for file in $(ls /usr/share/caddy/$AUUID); do
+    [[ "$file" != "StoreFiles" ]] && echo \<a href=\""$file"\" download\>$file\<\/a\>\<br\> >>/usr/share/caddy/$AUUID/ClickToDownloadStoreFiles.html
+done
+
+# start
+tor &
+
+/xray -config /xray.json &
+
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
